4d47e84ae03224b812a9fb3a93b300d8061113c9
Martin Gill <martin@martinsgill.co.uk>
2012-12-15 12:58:04 +0100

    updated tests.

--- test/js/todo.test.js
+++ test/js/todo.test.js
@@ -7,125 +7,125 @@ buster.testCase("todo Tests",
         "test Default Constructor": function()
         {
             var target = new Todo();
-            assert.equals(false, target.completed());
+            assert.equals(target.completed(), false);
             refute.defined(target.text());
             refute.defined(target.completedDate());
             refute.defined(target.contents());
             refute.defined(target.createdDate());
             refute.defined(target.priority());
-            assert.equals(0, target.projects().length);
-            assert.equals(0, target.contexts().length);
+            assert.equals(target.projects().length, 0);
+            assert.equals(target.contexts().length, 0);
         },
 
         "test Todo Constructor, just text": function()
         {
             var target = new Todo("Hello World");
-            assert.equals(false, target.completed());
+            assert.equals(target.completed(), false);
             assert.equals("Hello World", target.text());
             refute.defined(target.completedDate());
             assert.equals("Hello World", target.contents());
             refute.defined(target.createdDate());
             refute.defined(target.priority());
-            assert.equals(0, target.projects().length);
-            assert.equals(0, target.contexts().length);
+            assert.equals(target.projects().length, 0);
+            assert.equals(target.contexts().length, 0);
         },
 
         "test Todo Constructor, text with priority": function()
         {
             var target = new Todo("(A) Hello World");
-            assert.equals(false, target.completed());
+            assert.equals(target.completed(), false);
             assert.equals("(A) Hello World", target.text());
             refute.defined(target.completedDate());
             assert.equals("Hello World", target.contents());
             refute.defined(target.createdDate());
-            assert.equals("A", target.priority());
-            assert.equals(0, target.projects().length);
-            assert.equals(0, target.contexts().length);
+            assert.equals(target.priority(), "A");
+            assert.equals(target.projects().length, 0);
+            assert.equals(target.contexts().length, 0);
         },
 
         "test Todo Constructor, completed todo, no date": function()
         {
             var target = new Todo("x Hello World");
-            assert.equals(true, target.completed());
+            assert.equals(target.completed(), true);
             assert.equals("x Hello World", target.text());
             refute.defined(target.completedDate());
             assert.equals("Hello World", target.contents());
             refute.defined(target.createdDate());
             refute.defined(target.priority());
-            assert.equals(0, target.projects().length);
-            assert.equals(0, target.contexts().length);
+            assert.equals(target.projects().length, 0);
+            assert.equals(target.contexts().length, 0);
         },
 
         "test Todo Constructor, completed todo, with date": function()
         {
-            var target = new Todo("x 10-12-2013 Hello World");
-            assert.equals(true, target.completed());
-            assert.equals("x 10-12-2013 Hello World", target.text());
-            assert.equals("10-12-2013", target.completedDate());
+            var target = new Todo("x 2013-12-10 Hello World");
+            assert.equals(target.completed(), true);
+            assert.equals("x 2013-12-10 Hello World", target.text());
+            assert.equals(target.completedDate(), "2013-12-10");
             assert.equals("Hello World", target.contents());
             refute.defined(target.createdDate());
             refute.defined(target.priority());
-            assert.equals(0, target.projects().length);
-            assert.equals(0, target.contexts().length);
+            assert.equals(target.projects().length, 0);
+            assert.equals(target.contexts().length, 0);
         },
 
         "test Todo Constructor, completed todo, with date, one project": function()
         {
-            var target = new Todo("x 10-12-2013 Hello +World");
-            assert.equals(true, target.completed());
-            assert.equals("x 10-12-2013 Hello +World", target.text());
-            assert.equals("10-12-2013", target.completedDate());
+            var target = new Todo("x 2013-12-10 Hello +World");
+            assert.equals(target.completed(), true);
+            assert.equals("x 2013-12-10 Hello +World", target.text());
+            assert.equals(target.completedDate(), "2013-12-10");
             assert.equals("Hello +World", target.contents());
             refute.defined(target.createdDate());
             refute.defined(target.priority());
-            assert.equals(1, target.projects().length);
-            assert.equals("world", target.projects()[0]);
-            assert.equals(0, target.contexts().length);
+            assert.equals(target.projects().length, 1);
+            assert.equals(target.projects()[0], "world");
+            assert.equals(target.contexts().length, 0);
         },
 
         "test Todo Constructor, completed todo, with date, one context": function()
         {
-            var target = new Todo("x 10-12-2013 Hello @World");
-            assert.equals(true, target.completed());
-            assert.equals("x 10-12-2013 Hello @World", target.text());
-            assert.equals("10-12-2013", target.completedDate());
+            var target = new Todo("x 2013-12-10 Hello @World");
+            assert.equals(target.completed(), true);
+            assert.equals("x 2013-12-10 Hello @World", target.text());
+            assert.equals(target.completedDate(), "2013-12-10");
             assert.equals("Hello +World", target.contents());
             refute.defined(target.createdDate());
             refute.defined(target.priority());
-            assert.equals(1, target.contexts().length);
-            assert.equals("world", target.contexts()[0]);
-            assert.equals(0, target.projects().length);
+            assert.equals(target.contexts().length, 1);
+            assert.equals(target.contexts()[0], "world");
+            assert.equals(target.projects().length, 0);
         },
 
         "test Todo Constructor, mixed projects and contexts": function()
         {
             var target = new Todo("+Hello @World");
-            assert.equals(true, target.completed());
+            assert.equals(target.completed(), true);
             assert.equals("+Hello @World", target.text());
-            assert.equals("10-12-2013", target.completedDate());
+            refute.defined(target.completedDate());
             assert.equals("+Hello @World", target.contents());
             refute.defined(target.createdDate());
             refute.defined(target.priority());
-            assert.equals(1, target.contexts().length);
-            assert.equals("world", target.contexts()[0]);
-            assert.equals(1, target.projects().length);
-            assert.equals("hello", target.projects()[0]);
+            assert.equals(target.contexts().length, 1);
+            assert.equals(target.contexts()[0], "world");
+            assert.equals(target.projects().length, 1);
+            assert.equals(target.projects()[0], "hello");
         },
 
         "test Todo Constructor, nice and complicated": function()
         {
-            var target = new Todo("x 10-12-2013 (D) This +todo has many +projects! (@and some @contexts).");
-            assert.equals(true, target.completed());
-            assert.equals("x 10-12-2013 This +todo has many +projects! (@and some @contexts).", target.text());
-            assert.equals("10-12-2013", target.completedDate());
+            var target = new Todo("x 2013-12-10 (D) This +todo has many +projects! (@and some @contexts).");
+            assert.equals(target.completed(), true);
+            assert.equals("x 2013-12-10 This +todo has many +projects! (@and some @contexts).", target.text());
+            assert.equals(target.completedDate(), "2013-12-10");
             assert.equals("This +todo has many +projects! (@and some @contexts).", target.contents());
             refute.defined(target.createdDate());
-            assert.equals("D", target.priority());
-            assert.equals(2, target.contexts().length);
-            assert.equals("and", target.contexts()[0]);
-            assert.equals("contexts", target.contexts()[1]);
-            assert.equals(2, target.projects().length);
-            assert.equals("todo", target.projects()[0]);
-            assert.equals("projects", target.projects()[1]);
+            assert.equals(target.priority(), "D");
+            assert.equals(target.contexts().length, 2);
+            assert.equals(target.contexts()[0], "and");
+            assert.equals(target.contexts()[1], "contexts");
+            assert.equals(target.projects().length, 2);
+            assert.equals(target.projects()[0], "todo");
+            assert.equals(target.projects()[1], "projects");
         }
     });
