f78523c78a1e25fe858073d4698b38d8dcf523d0
Martin Gill <martin@martinsgill.co.uk>
2012-12-15 13:54:34 +0100

    attempt to fix buster dependecy issue

--- test/buster.js
+++ test/buster.js
@@ -4,10 +4,10 @@ config["My tests"] = {
     rootPath: "../",
     environment: "browser", // or "node"
     libs: [
-        "src/js/knockout/*.js",
         "src/js/jQuery/js/jquery.js",
-        "src/js/jQuery/js/jquery-ui.js",
-        "src/js/underscore/*.js"
+        "src/js/knockout/*.js",
+        "src/js/underscore/*.js",
+        "src/js/jQuery/js/jquery-ui.js"
     ],
     sources: [
         "src/js/todo.js"
