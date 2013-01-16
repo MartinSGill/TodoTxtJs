d73697765573bbd12b4f0f124e331bea8aff5e69
Martin Gill <martin@martinsgill.co.uk>
2013-01-16 17:41:22 +0100

    updated copyright to make this an MIT Licensed project.

--- src/js/todotxt.js
+++ src/js/todotxt.js
@@ -1,3 +1,26 @@
+/*******************************************************************************
+ * Copyright (C) 2013 Martin Gill
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ ******************************************************************************/
+
 ////////////////////////////////////////////////////////////////////////////
 // Main View Model
 ////////////////////////////////////////////////////////////////////////////
@@ -16,8 +39,8 @@ function TodoTxtViewModel()
     self.allTodos = ko.computed(function() { return todoManager.all(); } );
 
     self.priorities = ko.observableArray([]);
-    self.projects = ko.computed(function() { return todoManager.allProjects(); } );
-    self.contexts = ko.computed(function() { return todoManager.allContexts(); } );
+    self.projects = ko.computed(function() { return todoManager.allProjects().sort(); } );
+    self.contexts = ko.computed(function() { return todoManager.allContexts().sort(); } );
 
     self.showCompleted = ko.observable(false);
 
@@ -267,8 +290,20 @@ function TodoTxtViewModel()
         self.filters("");
     };
 
+    self.addFilterFromElement = function(newFilter)
+    {
+        var result = self.filters().trim();
+        if (self.filters().length > 0)
+        {
+            result += " ";
+        }
+        result += $(newFilter).text();
+        self.filters(result);
+    };
+
     self.addFilter = function(newFilter)
     {
+
         var result = self.filters().trim();
         if (self.filters().length > 0)
         {
