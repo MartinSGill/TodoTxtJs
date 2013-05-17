ceddc2349593b4270d480ce0fc8281dca5809b25
Martin Gill <martin@martinsgill.co.uk>
2013-05-17 20:26:49 +0200

    Fixes #28. Also added auto-complete for the filters box.

--- src/js/todotxt.js
+++ src/js/todotxt.js
@@ -578,10 +578,42 @@ function TodoTxtViewModel()
           terms.push( ui.item.value );
           // add placeholder to get the comma-and-space at the end
           terms.push( "" );
-          this.value = terms.join( " " );
+          self.newTodoText(terms.join( " " ));
           return false;
         }
       });
+
+    $( "#filters" )
+        // don't navigate away from the field on tab when selecting an item
+        .bind( "keydown", function( event ) {
+                   if ( event.keyCode === $.ui.keyCode.TAB &&
+                       $( this ).data( "ui-autocomplete" ).menu.active ) {
+                       event.preventDefault();
+                   }
+               })
+        .autocomplete({
+                          minLength: 1,
+                          source: function( request, response ) {
+                              // delegate back to autocomplete, but extract the last term
+                              response( $.ui.autocomplete.filter(
+                                  newTodoAutoCompleteValues(), extractLast( request.term ) ) );
+                          },
+                          focus: function() {
+                              // prevent value inserted on focus
+                              return false;
+                          },
+                          select: function( event, ui ) {
+                              var terms = split( this.value );
+                              // remove the current input
+                              terms.pop();
+                              // add the selected item
+                              terms.push( ui.item.value );
+                              // add placeholder to get the comma-and-space at the end
+                              terms.push( "" );
+                              self.filters(terms.join( " " ));
+                              return false;
+                          }
+                      });
 }
 
 var todoTxtView = new TodoTxtViewModel();
