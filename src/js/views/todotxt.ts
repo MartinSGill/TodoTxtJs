d0225e13fc9d430ecdc0af335d1c354336abba8f
Martin Gill <martin@martinsgill.co.uk>
2013-08-10 15:22:51 +0200

    Changed version number

--- src/js/views/todotxt.ts
+++ src/js/views/todotxt.ts
@@ -70,7 +70,7 @@ module TodoTxtJs.View
         {
             this._todoManager = new TodoTxtJs.TodoManager();
 
-            this.version = ko.observable<string>("1.0.0");
+            this.version = ko.observable<string>("1.0.1");
             this.title = ko.observable<string>("TodoTxtJs");
             this.allTodos = ko.computed({owner: this, read: this._getAllTodos});
             this.priorities = ko.observableArray([]);
