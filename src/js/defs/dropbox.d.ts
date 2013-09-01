6edf8c2a07b0aee0559a164e3471f8812f148073
Martin Gill <martin@martinsgill.co.uk>
2013-09-01 14:13:34 +0200

    Much expanded dropbox def

--- src/js/defs/dropbox.d.ts
+++ src/js/defs/dropbox.d.ts
@@ -21,17 +21,147 @@
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  ******************************************************************************/
 
-interface IDropboxApi
+declare module Dropbox
 {
-    Client(options: Object) : void;
-    Drivers: IDropboxDrivers;
+    module File
+    {
+        class Stat
+        {
+            public path: string;
+            public name: string;
+            public inAppFolder: boolean;
+            public isFolder: boolean;
+            public isFile: boolean;
+            public isRemoved: boolean;
+            public typeIcon: string;
+            public versionTag: string;
+            public contentHash: string;
+            public mimeType: string;
+            public size: number;
+            public humanSize: string;
+            public hasThumbnail: boolean;
+            public modifiedAt: Date;
+            public clientModifiedAt: Date;
+
+            public static parse(metadata: Object): Stat;
+            public json(): Object;
+        }
+    }
+
+    module Http
+    {
+        class RangeInfo
+        {
+            public start: number;
+            public size: number;
+            public end: number;
+
+            public static parse(headerValue: string): RangeInfo;
+        }
+    }
+
+    interface ClientOptions
+    {
+        key: string;
+        secret?: string;
+        token?: string;
+        uid?: string;
+    }
+
+    interface ClientAuthenticateOptions
+    {
+        interactive?: boolean;
+    }
+
+    interface ClientReadFileOptions
+    {
+        versionTag?: string;
+        rev?: string;
+        arrayBuffer?: boolean;
+        blob?: boolean;
+        buffer?: boolean;
+        length?: number;
+        start?: number;
+        httpCache?: boolean;
+    }
+
+    interface ClientWriteFileOptions
+    {
+        lastVersionTag: string;
+        parentRev: string;
+        noOverwrite: boolean;
+    }
+
+    class ApiError
+    {
+        constructor(xhr: XMLHttpRequest, method: string, url: string);
+
+        public status: number;
+        public method: string;
+        public url: string;
+        public responseText: string;
+        public response: Object;
+
+        public static NETWORK_ERROR: number;
+        public static NO_CONTENT: number;
+        public static INVALID_PARAM: number;
+        public static INVALID_TOKEN: number;
+        public static OAUTH_ERROR: number;
+        public static NOT_FOUND: number;
+        public static INVALID_METHOD: number;
+        public static NOT_ACCEPTABLE: number;
+        public static CONFLICT: number;
+        public static RATE_LIMITED: number;
+        public static SERVER_ERROR: number;
+        public static OVER_QUOTA: number;
+    }
+
+    class Client
+    {
+        constructor(options: ClientOptions);
+
+        public authDriver(driver: any): Client;
+        public authenticate(callback: (error: ApiError, client: Client) => void): Client;
+        public authenticate(options: ClientAuthenticateOptions, callback: (error: ApiError, client: Client) => void): Client;
+        public isAuthenticated(): boolean;
+
+        public readFile(path: string, options: ClientReadFileOptions, callback: (error: ApiError, contents: string, metadata: File.Stat, rangeInfo: Http.RangeInfo) => void): XMLHttpRequest;
+        public writeFile(path: string, data: any, options: ClientWriteFileOptions, callback: (error: ApiError, metadata: File.Stat) => void): XMLHttpRequest;
+    }
+
+    module Drivers
+    {
+        interface BrowserBaseOptions
+        {
+            scope?: string;
+            rememberUser?: boolean;
+        }
+
+        class BrowserBase
+        {
+            constructor(options?: BrowserBaseOptions);
+
+            authType(): void;
+            onAuthStepChange(client: Client, callback: Function): void;
+            locationStateParam(url: string): string;
+        }
+
+        interface RedirectOptions extends BrowserBaseOptions
+        {
+            redirectUrl?: string;
+            redirectFile?: string;
+        }
+
+        class Redirect extends BrowserBase
+        {
+            constructor(options?: RedirectOptions);
+
+            url(): string;
+            doAuthorize(authUrl: string, stateParam: string, client: Client): void;
+            resumeAuthorize(stateParam: string, client: Client, callback: Function): void;
+        }
+    }
 }
 
-interface IDropboxDrivers
-{
-    Redirect(options: Object) : void;
-}
-
-declare var Dropbox : IDropboxApi;
 declare var dropbox_key: string;
 
