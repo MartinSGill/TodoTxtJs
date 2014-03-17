5bd40dea03c92f32c2470143b97c7670c344c3ab
Martin Gill <martin@martinsgill.co.uk>
2014-03-17 22:11:56 +0100

    Updated version number. Along with previous commit this should now fix #67

--- src/js/views/todotxt.ts
+++ src/js/views/todotxt.ts
@@ -73,7 +73,7 @@ module TodoTxtJs.View
             this._todoManager = new TodoTxtJs.TodoManager();
 
             this._title = ko.observable<string>("TodoTxtJs");
-            this.version = ko.observable<string>("1.4.5");
+            this.version = ko.observable<string>("1.4.6");
             this.allTodos = ko.computed({owner: this, read: this._getAllTodos});
             this.priorities = ko.computed({owner: this, read: this._getAllPriorities});
             this.projects = ko.computed({owner: this, read: this._getAllProjects});
