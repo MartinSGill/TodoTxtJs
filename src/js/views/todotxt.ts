e07841c1dff42803f32411bea2513e0a01af4c7b
Martin Gill <martin@martinsgill.co.uk>
2014-04-23 15:31:27 +0200

    updated version number and features file.

--- src/js/views/todotxt.ts
+++ src/js/views/todotxt.ts
@@ -76,7 +76,7 @@ module TodoTxtJs.View
             this._todoManager = new TodoTxtJs.TodoManager();
 
             this._title = ko.observable<string>("TodoTxtJs");
-            this.version = ko.observable<string>("1.4.6");
+            this.version = ko.observable<string>("1.5.0");
             this.allTodos = ko.computed({owner: this, read: this._getAllTodos});
             this.priorities = ko.computed({owner: this, read: this._getAllPriorities});
             this.projects = ko.computed({owner: this, read: this._getAllProjects});
