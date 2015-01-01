7c627b1499c32e70c91e5684865169824b318931
Martin Gill <martin@martinsgill.co.uk>
2015-01-01 18:43:56 +0100

    some tidying up

--- build.ps1
+++ build.ps1
@@ -1,8 +1,8 @@
 
 Write-Host '** Building WebApp **'
 npm install --prefix ./WebApp ./WebApp
-WebApp/node_modules/.bin/grunt --gruntfile ./WebApp/GruntFile.js release
+WebApp/node_modules/.bin/grunt.cmd --gruntfile ./WebApp/GruntFile.js release
 
 Write-Host '** Server **'
 npm install --prefix ./TodoTxtJsServer ./TodoTxtJsServer
-TodoTxtJsServer/node_modules/.bin/grunt --gruntfile ./TodoTxtJsServer/GruntFile.js release
+TodoTxtJsServer/node_modules/.bin/grunt.cmd --gruntfile ./TodoTxtJsServer/GruntFile.js release
