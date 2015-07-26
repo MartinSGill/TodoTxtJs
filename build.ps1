6196fb36a55baeab1bc6ee7fb6ede19c15645512
Martin Gill <martin@martinsgill.co.uk>
2015-07-26 12:03:11 +0200

    removed the server project

--- build.ps1
+++
@@ -1,8 +0,0 @@
-
-Write-Host '** Building WebApp **'
-npm install --prefix ./WebApp ./WebApp
-WebApp/node_modules/.bin/grunt.cmd --gruntfile ./WebApp/GruntFile.js release
-
-Write-Host '** Server **'
-npm install --prefix ./TodoTxtJsServer ./TodoTxtJsServer
-TodoTxtJsServer/node_modules/.bin/grunt.cmd --gruntfile ./TodoTxtJsServer/GruntFile.js release
