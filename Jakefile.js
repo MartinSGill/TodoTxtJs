068f42f7c2ac799a5cde3c24022c8f74d0c70920
Martin Gill <martin@martinsgill.co.uk>
2015-08-06 20:34:31 +0200

    improved build tasks. All output is now to a single folder, will make creating a package easier in future

--- Jakefile.js
+++ Jakefile.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-var fs = require("fs");
-var os = require("os");
-var path = require("path");
+var os = require('os');
+var path = require('path');
+var out_path = 'out';
 
 var less_base = 'src/css';
 var less_files = [
@@ -12,27 +12,29 @@ var less_files = [
 ];
 
 var ts_base = './src/js/';
-var ts_files =
-    [
-        ts_base + 'model/contentRender.ts',
-        ts_base + 'model/ModelFactory.ts',
-        ts_base + 'model/options.ts',
-        ts_base + 'model/regex.ts',
-        ts_base + 'model/todo.ts',
-        ts_base + 'model/todomanager.ts',
-
-        ts_base + 'storage/storage_browser.ts',
-        ts_base + 'storage/storage_dropbox.ts',
-
-        ts_base + 'utils/datetime.ts',
-        ts_base + 'utils/events.ts',
-
-        ts_base + 'views/displayOptions.ts',
-        ts_base + 'views/exporting.ts',
-        ts_base + 'views/importing.ts',
-        ts_base + 'views/todotxt.ts'
-    ];
-var ts_outfile = 'app.js';
+var ts_files = new jake.FileList();
+ts_files.include('./src/js/**/*.ts');
+//var ts_files =
+//    [
+//        ts_base + 'model/contentRender.ts',
+//        ts_base + 'model/ModelFactory.ts',
+//        ts_base + 'model/options.ts',
+//        ts_base + 'model/regex.ts',
+//        ts_base + 'model/todo.ts',
+//        ts_base + 'model/todomanager.ts',
+//
+//        ts_base + 'storage/storage_browser.ts',
+//        ts_base + 'storage/storage_dropbox.ts',
+//
+//        ts_base + 'utils/datetime.ts',
+//        ts_base + 'utils/events.ts',
+//
+//        ts_base + 'views/displayOptions.ts',
+//        ts_base + 'views/exporting.ts',
+//        ts_base + 'views/importing.ts',
+//        ts_base + 'views/todotxt.ts'
+//    ];
+var ts_outfile = 'out/app.js';
 var ts_options = [
     '--out ' + ts_outfile,
     '--sourceMap',
@@ -41,30 +43,76 @@ var ts_options = [
     '--removeComments'
 ];
 
+var res_files = new jake.FileList();
+    res_files.include('src/images/dropbox-logos_dropbox-logotype-black-trimmed.png');
+    res_files.include('src/images/todotxt_js_logo.png');
+
+var sup_files = new jake.FileList();
+    sup_files.include('src/js/lib/jquery.hotkeys.js');
+    sup_files.include('src/js/bindings/binding_todo.js');
+    sup_files.include('src/js/sample_dropbox_key.js');
+    // Optional files
+    sup_files.include('src/js/dropbox_key.*');
+    sup_files.include('src/js/events.*');
+
+
 function compileLessFile(file) {
+    jake.logger.log('Compile LESS: ' + file);
     var in_file = path.join(less_base, file + '.less');
-    var out_file = path.join(less_base, file + '.css');
+    var out_file = path.join(out_path, 'css', file + '.css');
     var cmd = 'lessc --source-map ' + in_file + ' ' + out_file;
-    jake.exec(cmd, {interactive: true, breakOnError: true});
+    jake.exec(cmd, {printStdout: !jake.program.opts.quiet, breakOnError: true});
 }
 
-function compilteTsFiles() {
-    var files = ts_files.join(' ');
+function compileTsFiles() {
+    jake.logger.log('Compile TypeScript');
+    var files = ts_files.toArray().join(' ');
     var options = ts_options.join(' ');
     var cmd = "tsc " + options + ' ' + files;
-    jake.exec(cmd, {interactive: true, breakOnError: true});
+    jake.exec(cmd, {printStdout: !jake.program.opts.quiet, breakOnError: true});
 }
 
 desc('This is the default task.');
-task('default', ['less', 'ts']);
+task('default', ['less', 'ts', 'res', 'html', 'sup']);
 
-desc('Build TS files');
-task('ts', function (params) {
-    compilteTsFiles();
+desc('Ensure build artifacts folder exists.');
+task('out', function (params) {
+    jake.mkdirP(path.join(out_path, 'images'));
+    jake.mkdirP(path.join(out_path, 'css'));
 });
 
+desc('Build TS files');
+task('ts', ['out'], function (params) {
+    compileTsFiles();
+});
 
-desc('Build Less file');
-task('less', function (params) {
+desc('Build Less files');
+task('less', ['out'], function (params) {
     less_files.forEach(compileLessFile);
+});
+
+desc('Build/deploy resources');
+task('res', ['out'], function (params) {
+    jake.logger.log('Compile Resources');
+    res_files.toArray().forEach(function (file) {
+       var destination = path.join(out_path, 'images', path.basename(file));
+        jake.cpR(file, destination);
+    });
+});
+
+desc('Build/deploy supplementary JS files');
+task('sup', ['out'], function (params) {
+    jake.logger.log('Compile Supplemental Files');
+    sup_files.toArray().forEach(function (file) {
+        var destination = path.join(out_path, path.basename(file));
+        jake.cpR(file, destination);
+    });
+});
+
+desc('Build/deploy HTML');
+task('html', ['out'], function (params) {
+    jake.logger.log('Compile HTML');
+    var source = path.join('src', 'todotxt.html');
+    var destination = path.join(out_path, 'index.html');
+    jake.cpR(source, destination);
 });
