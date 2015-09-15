b0a806936f8c9d601caca1b885724e5c963f4b40
Martin Gill <martin@martinsgill.co.uk>
2015-09-15 20:40:17 +0200

    simple angular structure created from an example

---
+++ src/controllers/to-do-list-controller.ts
@@ -0,0 +1,58 @@
+/*******************************************************************************
+ * Copyright (C) 2013-2015 Martin Gill
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ ******************************************************************************/
+
+/// <reference path="../typings/tsd.d.ts" />
+/// <reference path="../directives/to-do-list.ts" />
+/// <reference path="../models/list-item.ts" />
+
+namespace TodoTxt.Controllers
+{
+    export class ToDoListController {
+        name: string;
+        listItems: ListItem[];
+
+        newItemName: string;
+
+        static $inject = [
+            "$scope"
+        ];
+        constructor(isolateScope: Directives.IToDoListScope) {
+            this.name = isolateScope.name;
+            this.listItems = [];
+        }
+
+        save() {
+            if (this.newItemName && this.newItemName.length > 0) {
+                var newItem = new ListItem(this.newItemName);
+                this.listItems.push(newItem);
+
+                this.newItemName = null;
+            }
+        }
+
+        toggle(listItem: ListItem): boolean {
+            listItem.isComplete = !listItem.isComplete;
+            return listItem.isComplete;
+        }
+    }
+}
