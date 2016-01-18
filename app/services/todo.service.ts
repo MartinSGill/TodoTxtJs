aa9d99f1c35f74912758cc4b0581cdbe20422000
Martin Gill <martin@martinsgill.co.uk>
2016-01-18 20:10:50 +0100

    moved list model into service

--- app/services/todo.service.ts
+++ app/services/todo.service.ts
@@ -11,13 +11,47 @@ export class TodoService {
         this.items = TODOS.slice(0,3);
     }
 
-    all() {
+    public nextIndex():number {
+        return this.items.length;
+    }
+
+    public all():Item[] {
         return this.items;
     }
 
-    add(item:Item) {
-        if (item.tokens().length > 0) {
+    public add(item:Item):void {
+        if ((item instanceof Item) && item.tokens().length > 0) {
+            item.index = this.nextIndex();
             this.items.push(item);
         }
+        else {
+            throw 'invalid item. Expecting Item object';
+        }
+    }
+
+    public projects():string[] {
+        return this.items.reduce(function (previousValue, currentValue) {
+                return previousValue.concat(currentValue.projects());
+            }, [])
+            .sort()
+            .filter(function (item, index, items) {
+                if (index == 0) {
+                    return true;
+                }
+                return items[index - 1] != item;
+            });
+    }
+
+    public contexts():string[] {
+        return this.items.reduce(function (previousValue, currentValue) {
+                return previousValue.concat(currentValue.contexts());
+            }, [])
+            .sort()
+            .filter(function (item, index, items) {
+                if (index == 0) {
+                    return true;
+                }
+                return items[index - 1] != item;
+            });
     }
 }
